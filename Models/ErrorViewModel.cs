c6c9ec55eb0149a8c0072150a56c50eb31b7dc65
STUDENT <STUDENT@DESKTOP-JJ1HVGS>
2024-03-02 13:12:24 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E2_PulganMyrazaRiann_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
